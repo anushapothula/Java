b343e6999faa0668526c5e52fee6ccdc452d1b87
anushapothula <83360113+anushapothula@users.noreply.github.com>
2021-04-30 14:26:48 +0530

    Update and rename README.md to Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+
+class Factorial{
+public statuc void main(String[] args){ 
+Scanner sc=new Scanner(System.in); 
+Syste.out.println("enter the number"); 
+int n=sc.nextInt();
+ int fact=1; 
+for(int i=1;i&lt;=n;i++){
+ fact=fact*1;
+ } 
+System.out.println(fact); 
+} 
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Java
-class Factorial{ public statuc void main(String[] args){ Scanner sc=new Scanner(System.in); Syste.out.println("enter the number"); int n=sc.nextInt(); int fact=1; for(int i=1;i&lt;=n;i++){ fact=fact*1; } System.out.println(fact); } }
